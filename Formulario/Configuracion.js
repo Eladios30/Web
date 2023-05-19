27e08ac8a1d9a35dbfd6e0b6ed3c5e450ad19739
Eladios30 <eladioquien@gmail.com>
2023-05-18 21:35:50 -0700

    Cambios en formulario, configuracion del servidor

---
+++ Formulario/Configuracion.js
@@ -0,0 +1,15 @@
+//Obteniendo datos de la configuracion de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCqNvZMTLyG4U2UmpVfvP3OChtJVgxM0mU",
+    authDomain: "educarte-3bc4b.firebaseapp.com",
+    projectId: "educarte-3bc4b",
+    storageBucket: "educarte-3bc4b.appspot.com",
+    messagingSenderId: "175450238774",
+    appId: "1:175450238774:web:a5a261932cf3d07ade8d97"
+};
+
+//Inicializando Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Inicializando Cloud Firestore y obteniendo una referencia del servicio
+var db = firebase.firestore();
--- Formulario/obtener.js
+++ Formulario/obtener.js
@@ -1,62 +1,38 @@
+//Mensajes de exito
+const mensajeExitoso = () => {
+  Swal.fire(
+    'Buen trabajo!',
+    'You clicked the button!',
+    'Realizado'
+  )
+}
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyCqNvZMTLyG4U2UmpVfvP3OChtJVgxM0mU",
-      authDomain: "educarte-3bc4b.firebaseapp.com",
-      projectId: "educarte-3bc4b",
-      storageBucket: "educarte-3bc4b.appspot.com",
-      messagingSenderId: "175450238774",
-      appId: "1:175450238774:web:a5a261932cf3d07ade8d97"
+//Mensaje de error
+const mensajeError = () => {
+  Swal.fire({
+    icon: 'Error!',
+    title: 'Oops...',
+    text: 'Something went wrong!',
+    footer: '<a href="">Why do I have this issue?</a>'
+  })
+}
 
-      // Initialize Cloud Firestore and get a reference to the service
-    };
-
-        // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-
-    // Initialize Cloud Firestore and get a reference to the service
-    var db = firebase.firestore();
-
-    const guardarDatos = (user) => {
-    db.collection("usuarios").add({
-        user,
-    })
+//Funcion para guardar los datos y enviarlo al servidor de Firebase
+function guardar() {
+  db.collection("usuarios").add({
+    nombre: document.getElementById("nombre").value,
+    correo: document.getElementById("correo").value,
+    numero: document.getElementById("telefono").value,
+    fecha: document.getElementById("fecha").value,
+    plataforma: document.getElementById("correo").value,
+    red: document.getElementById("red_social").value,
+    nacionalidad: document.getElementById("nacional").value,
+    comentarios: document.getElementById("comentarios").value,
+  })
     .then((docRef) => {
       mensajeExitoso();
     })
     .catch((error) => {
       mensajeError();
     });
-  }
-
-  const mensajeExitoso = () => {
-    Swal.fire(
-      'Good job!',
-      'You clicked the button!',
-      'success'
-    )
-  }
-
-  const mensajeError = () => {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Something went wrong!',
-      footer: '<a href="">Why do I have this issue?</a>'
-    })
-  }
-
-  $("#guardar").on("click", () =>{
-    let nombre = $("$nombre").val();
-    let correo = $("$correo").val();
-    let numero = $("$numero").val();
-    let fecha =$("$fecha").val();
-
-    const user = {
-      nombre,
-      correo,
-      numero,
-      fecha
-    }
-    guardarDatos(user);
-  })
+}
