71d9a8b47704538db78165009b06fdc3822bd7b1
Eladios30 <eladioquien@gmail.com>
2023-05-18 17:51:36 -0700

    Enviando datos a un servidor

--- Formulario/obtener.js
+++ Formulario/obtener.js
@@ -13,5 +13,25 @@ function enviarFormulari(){
     formData.append("fecha", fecha);
 
     var solicitud = new XMLHttpRequest();
-    solicitud.open("POST", )
+    // Crear una solicitud HTTP (POST) hacia el servidor
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://credu.000webhostapp.com/", true);
+
+    // Configurar el encabezado de la solicitud (si es necesario)
+    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Manejar la respuesta del servidor (opcional)
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // Éxito: la solicitud fue exitosa
+        console.log("Lo logramos alv");
+        console.log(xhr.responseText);
+      } else {
+        // Error: la solicitud falló
+        console.error(xhr.statusText);
+      }
+    };
+
+    // Enviar la solicitud con los datos del formulario
+    xhr.send(formData);
 }
