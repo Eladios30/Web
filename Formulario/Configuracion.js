2cfd51728b4f35df0d9796d333206e17fd1f2c5f
Eladios30 <eladioquien@gmail.com>
2023-05-20 19:53:48 -0700

    Ya quedo, al fin

--- Formulario/Configuracion.js
+++ Formulario/Configuracion.js
@@ -1,16 +1,42 @@
 //Obteniendo datos de la configuracion de Firebase
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+firebase.initializeApp({
+  apiKey: "AIzaSyCqNvZMTLyG4U2UmpVfvP3OChtJVgxM0mU",
+  authDomain: "educarte-3bc4b.firebaseapp.com",
+  databaseURL: "https://educarte-3bc4b-default-rtdb.firebaseio.com",
+  projectId: "educarte-3bc4b",
+  storageBucket: "educarte-3bc4b.appspot.com",
+  messagingSenderId: "175450238774",
+  appId: "1:175450238774:web:a5a261932cf3d07ade8d97"
+});
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCqNvZMTLyG4U2UmpVfvP3OChtJVgxM0mU",
-    authDomain: "educarte-3bc4b.firebaseapp.com",
-    projectId: "educarte-3bc4b",
-    storageBucket: "educarte-3bc4b.appspot.com",
-    messagingSenderId: "175450238774",
-    appId: "1:175450238774:web:a5a261932cf3d07ade8d97"
-};
+// Initialize Firebase
+var db = firebase.database();
 
-//Inicializando Firebase
-firebase.initializeApp(firebaseConfig);
 
-//Inicializando Cloud Firestore y obteniendo una referencia del servicio
-var db = firebase.firestore();
+/*
+  var messagesRef = firebase.database().ref("Messages");
+//Funcion para guardar los datos y enviarlo al servidor de Firebase
+function guardar() {
+    var nombre = document.getElementById("nombre").value
+    var correo = document.getElementById("correo").value
+    var numero = document.getElementById("telefono").value
+    var fecha = document.getElementById("fecha").value
+    var plataforma = document.getElementById("correo").value
+    var red = document.getElementById("red_social").value
+    var nacionalidad = document.getElementById("nacional").value
+    var comentarios = document.getElementById("comentarios").value
+
+    database.ref("Information/" + nombre).set({
+      nombre : nombre,
+      correo : correo,
+      numero : numero,
+      fecha : fecha,
+      plataforma : plataforma,
+      red : red,
+      nacionalidad : nacionalidad,
+      comentarios : comentarios
+    })
+    alert("guardado");
+}*/
--- Formulario/obtener.js
+++ Formulario/obtener.js
@@ -17,69 +17,42 @@ const mensajeError = () => {
   })
 }
 
-//Funcion para guardar los datos y enviarlo al servidor de Firebase
-function guardar() {
-  db.collection("usuarios").add({
-    nombre: document.getElementById("nombre").value,
-    correo: document.getElementById("correo").value,
-    numero: document.getElementById("telefono").value,
-    fecha: document.getElementById("fecha").value,
-    plataforma: document.getElementById("correo").value,
-    red: document.getElementById("red_social").value,
-    nacionalidad: document.getElementById("nacional").value,
-    comentarios: document.getElementById("comentarios").value,
-  })
-    .then((docRef) => {
-      mensajeExitoso();
+function guardar(){
+
+    var nombre = document.getElementById("nombre").value
+    var correo = document.getElementById("correo").value
+    var numero = document.getElementById("telefono").value
+    var fecha = document.getElementById("fecha").value
+    var plataforma = document.getElementById("correo").value
+    var red = document.getElementById("red").value
+    var nacionalidad = document.getElementById("nacionalidad").value
+    var comentarios = document.getElementById("comentarios").value
+
+    db.ref("Information/" + nombre).set({
+      nombre : nombre,
+      correo : correo,
+      numero : numero,
+      fecha : fecha,
+      plataforma : plataforma,
+      red : red,
+      nacionalidad : nacionalidad,
+      comentarios : comentarios
     })
-    .catch((error) => {
-      mensajeError();
-    });
-}
-//Metiendo los datos a una tabla NO SQL
 /*
-var firebaseRef = firebase.database().ref("donadores/1");
-
-function insertando(){
-  set(ref(db, "Donadores/"+ numero),{
+  db.collection("usuarios").add({
     nombre : document.getElementById("nombre").value,
-    correo: document.getElementById("correo").value,
-    numero: document.getElementById("telefono").value,
-    fecha: document.getElementById("fecha").value,
-    plataforma: document.getElementById("correo").value,
-    red: document.getElementById("red_social").value,
-    nacionalidad: document.getElementById("nacional").value,
-    comentarios: document.getElementById("comentarios").value,
-  })
-  .then((docRef) => {
+    correo : document.getElementById("correo").value,
+    numero : document.getElementById("telefono").value,
+    fecha : document.getElementById("fecha").value,
+    plataforma : document.getElementById("plataforma").value,
+    red : document.getElementById("red").value,
+    nacionalidad : document.getElementById("nacionalidad").value,
+    comentarios : document.getElementById("comentarios").value
+})*/
+.then((docRef) => {
     mensajeExitoso();
-  })
-  .catch((error) => {
+})
+.catch((error) => {
     mensajeError();
-  });
+});
 }
-
-send.addEventListener("click", insertando);
-/*
-//Seleccionando el id y agregando evento
-document.querySelector("#send").addEventListener("click", () => {
-  //Declarando variables
-  const nombre = document.getElementById("nombre").value;
-  const correo = document.getElementById("correo").value;
-  const numero = document.getElementById("telefono").value;
-  const fecha = document.getElementById("fecha").value;
-  const plataforma = document.getElementById("correo").value;
-  const red = document.getElementById("red_social").value;
-  const nacionalidad = document.getElementById("nacional").value;
-  const comentarios = document.getElementById("comentarios").value;
-
-  //Pusheando las variables
-  firebaseRef.push(nombre);
-  firebaseRef.push(correo);
-  firebaseRef.push(numero);
-  firebaseRef.push(fecha);
-  firebaseRef.push(plataforma);
-  firebaseRef.push(red);
-  firebaseRef.push(nacionalidad);
-  firebaseRef.push(comentarios);
-});*/
