dc9c5b8291b0609e547e43f761f0bf4bcacad427
Eladios30 <eladioquien@gmail.com>
2023-05-27 22:30:33 -0700

    Pagina terminada

--- Formulario/obtener.js
+++ Formulario/obtener.js
@@ -23,25 +23,39 @@ function guardar() {
   //Declaramos variables
   var nombre = document.getElementById("nombre").value
   var correo = document.getElementById("correo").value
-  var numero = document.getElementById("telefono").value
-  var fecha = document.getElementById("fecha").value
-  var plataforma = document.getElementById("correo").value
-  var red = document.getElementById("red").value
-  var nacionalidad = document.getElementById("nacionalidad").value
-  var comentarios = document.getElementById("comentarios").value
 
-  db.ref("Counter/node").set({
-    contador : i
-  })
-  //Agregamos directorio para los nodos
-  db.ref("Information/" + i++).set({
-    nombre: nombre,
-    correo: correo,
-    numero: numero,
-    fecha: fecha,
-    plataforma: plataforma,
-    red: red,
-    nacionalidad: nacionalidad,
-    comentarios: comentarios
-  })
+  //Validamos si el correo sea veridico
+  if (validaciones(correo) == true) {
+    var numero = document.getElementById("telefono").value
+    var fecha = document.getElementById("fecha").value
+    var plataforma = document.getElementById("correo").value
+    var red = document.getElementById("red").value
+    var nacionalidad = document.getElementById("nacionalidad").value
+    var comentarios = document.getElementById("comentarios").value
+
+    //Introducimos valores a la base NO SQL
+    db.ref("Counter/node").set({
+      contador : i
+    })
+
+    //Agregamos directorio para los nodos
+    db.ref("Information/" + i++).set({
+      nombre: nombre,
+      correo: correo,
+      numero: numero,
+      fecha: fecha,
+      plataforma: plataforma,
+      red: red,
+      nacionalidad: nacionalidad,
+      comentarios: comentarios
+    })
+  } else {
+    alert("Vueva a escribir un correo valido");
+  }
+}
+
+//Funcion para validar correo
+function validaciones(correo){
+    var expresionRegular = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return expresionRegular.test(correo);
 }
